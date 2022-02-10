411edcc29c6c6db44019604ca3210eea1deaef79
mikseros <mmadynski@gmail.com>
2022-02-10 09:53:43 +0100

    Logout Frontend

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -108,10 +108,12 @@ export default {
     const router = useRouter();
 
     function logout() {
-      store.commit('logout');
-      router.push({
-        name: 'Login'
-      })
+      store.dispatch('logout')
+        .then(() => {
+          router.push({
+            name: 'Login'
+          });
+        });
     }
 
     return {
