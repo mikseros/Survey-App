950effbac44262310deb96b77f9210d6cc31fc4d
mikseros <mmadynski@gmail.com>
2022-02-10 10:54:58 +0100

    Surveys Structure

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,12 +1,125 @@
 import {createStore} from "vuex";
 import axiosClient from "../axios";
 
+const tmpSurveys = [
+    {
+        id: 100,
+        title: "Survet with no sense.",
+        slug: "no-sense-survey",
+        status: "draft",
+        image: "./img/rocket.jpg",
+        description: "Super Survey without any sense. Enjoy!",
+        created_at: "2022-02-10 10:06:35",
+        updated_at: "2022-02-10 10:06:35",
+        expire_date: "2022-02-20 10:06:35",
+        questions: [
+            {
+                id: 1,
+                type: "select",
+                question: "What is your country?",
+                description: null,
+                data: {
+                    options: [
+                        {
+                            uuid: "f8af96f2-1d80-4632-9e9e-b560670e52ea",
+                            text: "USA"
+                        },
+                        {
+                            uuid: "201c1ff5-23c9-42f9-bfb5-bbc850535440",
+                            text: "France"
+                        },
+                        {
+                            uuid: "b5c09733-a49e-460a-ba8a-526963010729",
+                            text: "United Kingdom"
+                        },
+                        {
+                            uuid: "2abf1cee-f5fb-427c-a220-b5d159ad6513",
+                            text: "Germany"
+                        },
+                        {
+                            uuid: "8d14341b-ec2b-4924-9aea-bda6a53b51fc",
+                            text: "Poland"
+                        },
+                    ],
+                },
+            },
+            {
+                id: 2,
+                type: "checkbox",
+                question: "What is your favorite programming language?",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a dui auctor, rhoncus enim et, finibus leo. Proin posuere non nibh eget congue. Etiam tempor.",
+                data: {
+                    options: [
+                        {
+                            uuid: "f9af96f2-2d80-4632-7e9e-b560670e52ea",
+                            text: "Java"
+                        },
+                        {
+                            uuid: "291g1ff5-23c9-12f9-cfb5-bac850536443",
+                            text: "PHP"
+                        },
+                        {
+                            uuid: "f2vf06f2-1k81-4612-9e8e-p560070a52ev",
+                            text: "JavaScript"
+                        },
+                        {
+                            uuid: "d3ax96s1-8b61-5142-8c9e-d123670e52yo",
+                            text: "Python"
+                        },
+                    ]
+                }
+            },
+            {
+                id: 3,
+                type: "radio",
+                question: "What kind of animal would you like to be?",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam a dui auctor, rhoncus enim et, finibus leo. Proin posuere non nibh eget congue. Etiam tempor.",
+                data: {
+                    options: [
+                        {
+                            uuid: "s1bf86t3-5d43-2612-3e0a-n378691a01eae",
+                            text: "Horse"
+                        },
+                        {
+                            uuid: "101h3ff4-33a8-10j8-ldx3-cba950536443",
+                            text: "Pig"
+                        },
+                        {
+                            uuid: "c4nm96f2-3h82-5715-0m9z-q479903s52ev",
+                            text: "Cow"
+                        },
+                        {
+                            uuid: "l3mx06e1-7v41-1146-7b0a-s247998a02ea",
+                            text: "Duck"
+                        },
+                    ]
+                }
+            },
+            {
+                id: 4,
+                type: "text",
+                question: "Are you vaccinated?",
+                description: null,
+                data: {},
+            },
+            {
+                id: 5,
+                type: "textarea",
+                question: "What do you think about moon landing?",
+                description: "Write you honest opinion. Nothing here is anonymous.",
+                data: {},
+            },
+        ],
+    },
+];
+
 const store = createStore({
     state: {
         user: {
             data: {},
             token: sessionStorage.getItem('TOKEN'),
         },
+        surveys: [tmpSurveys],
     },
     getters: {},
     actions: {
