85503aa103e37875b045e8345f462dfc38f50f20
mikseros <mmadynski@gmail.com>
2022-02-10 09:45:39 +0100

    Logout Backend

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::post('/register', [AuthController::class, 'register']);
