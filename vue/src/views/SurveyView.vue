251b76af107cd683dc2c0c7b86bcb3b641690cf1
mikseros <mmadynski@gmail.com>
2022-02-10 16:14:30 +0100

    Starting with Questions

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -145,6 +145,41 @@
                     <!--/ Status -->
                 </div>
                 <!--/ Survey Fields -->
+
+                <div class="px-4 py-5 bg-white space-y-6 sm:p-6">
+                    <h3 class="text-2xl font-sembibold flex items-center justify-between">
+                        Questions
+
+                        <!-- Add new question -->
+                        <button
+                            type="button"
+                            @click="addQuestion()"
+                            class="
+                                flex items-center text-sm py-1 px-4 rounded-sm
+                                text-white bg-gray-600 hover:bg-gray-700
+                            "
+                        >
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
+                            </svg>
+                            Add Question
+                        </button>
+                        <!--/ Add new question -->
+                    </h3>
+                    <div v-if="!model.questions.length" class="text-center text-gray-600">
+                        You don't have any questions yet!
+                    </div>
+                    <div v-for="(question, index) in model.questions" :key="question.id">
+                        <QuestionEditor
+                            :question="question"
+                            :index="index"
+                            @change="questionChange"
+                            @addQuestion="addQuestion"
+                            @deleteQuestion="deleteQuestion"
+                        />
+                    </div>
+                </div>
+
                 <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
                         type="submit"
