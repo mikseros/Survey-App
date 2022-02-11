a504af6715b51379dccb1c0adb54a80ee97d5c3b
mikseros <mmadynski@gmail.com>
2022-02-11 22:54:57 +0100

    Survey CRUD

--- vue/src/views/SurveyView.vue
+++ vue/src/views/SurveyView.vue
@@ -242,6 +242,15 @@ function deleteQuestion(question) {
         (q) => q.id !== question.id
     );
 }
+
+function questionChange(question) {
+    model.value.questions = model.value.questions.map((q) => {
+        if (q.id === question.id) {
+            return JSON.parse(JSON.stringify(question));
+        }
+        return q;
+    });
+}
 </script>
 
 <style>
